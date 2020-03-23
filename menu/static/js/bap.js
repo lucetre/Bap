e0eb0ec8f7bf6ae3adfe6e4dbb754142a654c915
jkgf6532 <jkgf6532@gmail.com>
2020-03-23 06:47:23 +0000

    Randomize within time seed

--- menu/static/js/bap.js
+++ menu/static/js/bap.js
@@ -1,12 +1,14 @@
 
 
 function bap() {
+    random.seed(a=None)
     console.log('!!RUBIS dinner!! v. 2 \n');
     let menu1 = '';
     let menu2 = '';
-    let a = Math.floor(Math.random() * 8) + 1;
-    switch(a) {
+    let amy = Math.floor(Math.random() * 8) + 1;
+    switch(amy) {
         case 1:  // Hansik
+            random.seed(a=None)
             b = Math.floor(Math.random() * 2) + 1;
             menu1 += 'The menu for today is Hansik:';
             switch(b) {
@@ -15,6 +17,7 @@ function bap() {
             }
             break;
         case 2:  // Bunsik
+            random.seed(a=None)
             b = Math.floor(Math.random() * 2) + 1;
             menu1 += 'The menu for today is Bunsik:';
             switch(b) {
@@ -23,6 +26,7 @@ function bap() {
             }
             break;
         case 3:  // Gogi
+            random.seed(a=None)
             b = Math.floor(Math.random() * 5) + 1;
             menu1 += 'The menu for today is Gogi:';
             switch(b) {
@@ -34,6 +38,7 @@ function bap() {
             }
             break;
         case 4:  // Joongsik
+            random.seed(a=None)
             b = Math.floor(Math.random() * 1) + 1;
             menu1 += 'The menu for today is Joongsik:';
             switch(b) {
@@ -41,6 +46,7 @@ function bap() {
             }
             break;
         case 5:  // Chicken
+            random.seed(a=None)
             b = Math.floor(Math.random() * 3) + 1;
             menu1 += 'The menu for today is Chicken:';
             switch(b) {
@@ -50,6 +56,7 @@ function bap() {
             }
             break;
         case 6:  // Pizza
+            random.seed(a=None)
             b = Math.floor(Math.random() * 2) + 1;
             menu1 += 'The menu for today is Pizza:';
             switch(b) {
@@ -58,6 +65,7 @@ function bap() {
             }
         break;
         case 7:  // Fastfood
+            random.seed(a=None)
             b = Math.floor(Math.random() * 2) + 1;
             menu1 += 'The menu for today is Fastfood:';
             switch(b) {
@@ -66,6 +74,7 @@ function bap() {
             }
             break;
         case 8:  // etc
+            random.seed(a=None)
             b = Math.floor(Math.random() * 2) + 1;
             menu1 += 'The menu for today is Special:';
             switch(b) {
